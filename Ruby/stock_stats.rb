835beed9d0bd9b9dcb98b844ea3369542ea2f868
Rajeev Vadalamudi <vprajeev86@gmail.com>
2013-10-25 06:56:49 -0700

    deleted stocks_stat.rb

--- Ruby/stock_stats.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'csv_reader'
-
-reader = CSVReader.new
-
-ARGV.each do |csv_file_name|
-	reader.csv_data_reader(csv_file_name)
-end
-
-puts "Total value = #{reader.total_value_in_stock}"
