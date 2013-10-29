0fd22b9eccdb9969c09bf3bec97f52b061663f98
Rajeev Vadalamudi <vprajeev86@gmail.com>
2013-10-29 05:07:46 -0700

    just another class example

---
+++ Ruby/Test/cpoly.rb
@@ -0,0 +1,21 @@
+class Cpoly
+	attr_accessor :width , :height
+	def initialize(width, height)
+		@width = width
+		@height = height
+	end
+
+	def area()
+		@width * @height
+	end
+
+end
+
+puts "enter the sides of the poly : "
+arr = gets.chomp
+(a,b) = arr.split("")
+triangle = Cpoly.new(a.to_i, b.to_i)
+puts "#{triangle.width} * #{triangle.height} = #{triangle.area}"
+
+triangle.height = 7
+puts "#{triangle.width} * #{triangle.height} = #{triangle.area}"
